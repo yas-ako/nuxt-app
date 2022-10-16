5c2bf99eaa255f833494e97cacade9cfda8cfe35
yas-ako <105139975+yas-ako@users.noreply.github.com>
2022-10-16 23:04:10 +0900

    [add] returnGroup.vue 団体紹介ページのためのAPI

---
+++ pages/groups/[name].vue
@@ -0,0 +1,13 @@
+<script>
+
+const route = useRoute();
+const name = route.query;
+const { article, fetchArticle } = useArticles();
+fetchArticle(+id);
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>

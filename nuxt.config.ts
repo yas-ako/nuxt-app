e3c5e873bd448e524af6222e106ed6f89d93b00a
yas-ako <105139975+yas-ako@users.noreply.github.com>
2022-10-16 20:51:21 +0900

    [clean] 不要なコメントアウトを削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
   css: [
     '@/assets/css/reset.css',
     '@/assets/css/style.css'
   ],
-  // app: {
     head: {
       meta: [      // <meta name="viewport" content="width=device-width, initial-scale=1">
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }]
     }
-  // }
 })

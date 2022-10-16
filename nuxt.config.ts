ba1ce3207175db92b7adbd327eedf08043644bec
yas-ako <105139975+yas-ako@users.noreply.github.com>
2022-10-16 15:26:48 +0900

    [update] nuxt.config.ts Google Fonts を導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
-  css:[
+  css: [
     '@/assets/css/reset.css',
     '@/assets/css/style.css'
-  ]
+  ],
+  // app: {
+    head: {
+      meta: [      // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }]
+    }
+  // }
 })

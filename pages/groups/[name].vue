c2188c0275758a97f032d25caa351743e583e4c7
yas-ako <105139975+yas-ako@users.noreply.github.com>
2022-10-17 22:40:56 +0900

    [delete] returnGroups.js

--- pages/groups/[name].vue
+++ pages/groups/[name].vue
@@ -1,13 +1,3 @@
-<script>
-
-const route = useRoute();
-const name = route.query;
-const { article, fetchArticle } = useArticles();
-fetchArticle(+id);
-</script>
-
 <template>
-  <div>
-
-  </div>
+  
 </template>
